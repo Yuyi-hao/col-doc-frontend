6c697f1e08e9f2f4e9b0475f81ed4519342f36a0
Yuyi-Hao <roshanisingh2354@gmail.com>
2025-02-08 19:47:06 +0530

    [FIX] fixes classname

--- src/pages/accounts/Profile.jsx
+++ src/pages/accounts/Profile.jsx
@@ -1,6 +1,7 @@
 import NavBar from "../../components/navbar";
 import { useState, useEffect } from "react";
 import api from "../../api";
+import { naturalTime } from "../../utils";
 function ProfileView(){
     const [user, setUser] = useState("");
     const getProfile = async () => {
@@ -26,43 +27,43 @@ function ProfileView(){
                     <img src={user.profile_pic} alt="" />
                 </div>
                 <div className="">
-                <table class="table table-borderless">
+                <table className="table table-borderless">
                     <tbody>
+                        <tr>
+                            <th scope="row">Email</th>
+                            <td>{user.email}</td>
+                        </tr> 
                         <tr>
                             <th scope="row">Name</th>
                             <td>{user.name}</td>
                         </tr>
                         <tr>
-                            <th scope="row">Date Of Birth</th>
-                            <td>{user.date_of_birth}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Last_Login</th>
-                            <td>{user.last_login}</td>
-                        </tr> 
-                        <tr>
-                            <th scope="row">Created_At</th>
-                            <td>{user.created_at}</td>
-                        </tr> 
-                        <tr>
-                            <th scope="row">Modified_At</th>
-                            <td>{user.modified_at}</td>
-                        </tr> 
-                        <tr>
                             <th scope="row">Nickname</th>
                             <td>{user.nickname}</td>
-                        </tr> 
-                        <tr>
-                            <th scope="row">Email</th>
-                            <td>{user.email}</td>
                         </tr> 
                         <tr>
                             <th scope="row">Description</th>
                             <td>{user.description}</td>
                         </tr> 
                         <tr>
+                            <th scope="row">Date Of Birth</th>
+                            <td>{user.date_of_birth}</td>
+                        </tr>
+                        <tr>
                             <th scope="row">Location</th>
                             <td>{user.location}</td>
+                        </tr> 
+                        <tr>
+                            <th scope="row">Last_Login</th>
+                            <td>{naturalTime(user.last_login)}</td>
+                        </tr> 
+                        <tr>
+                            <th scope="row">Created_At</th>
+                            <td>{naturalTime(user.created_at)}</td>
+                        </tr> 
+                        <tr>
+                            <th scope="row">Modified_At</th>
+                            <td>{naturalTime(user.modified_at)}</td>
                         </tr> 
                     </tbody>
                 </table>
@@ -70,27 +71,27 @@ function ProfileView(){
             </div>
             <h1 className="text-center">Document</h1>
             <div className="container d-flex flex-row flex-wrap justify-content-between">
-                <div className="card mb-3" style={{"max-width": "20rem"}}>
+                <div className="card mb-3" style={{"maxWidth": "20rem"}}>
                     <div className="card-header text-center ">Your Documents</div>
                     <div className="card-body text-success">
                         <h1 className="text-center">123</h1>
                     </div>
-                    <div class="card-footer bg-transparent">You have created 123 Documents</div>
+                    <div className="card-footer bg-transparent">You have created 123 Documents</div>
                 </div>
-                <div className="card mb-3" style={{"max-width": "20rem"}}>
+                <div className="card mb-3" style={{"maxWidth": "20rem"}}>
                     <div className="card-header text-center">Shared Documents</div>
                     <div className="card-body text-success">
                         <h1 className="text-center">123</h1>
                     </div>
-                    <div class="card-footer ">You share 123 Documents</div>
+                    <div className="card-footer ">You share 123 Documents</div>
                 </div>
-                <div className="card mb-3" style={{"max-width": "20rem"}}>
+                <div className="card mb-3" style={{"maxWidth": "20rem"}}>
                     <div className="card-header text-center  ">Storage</div>
                     <div className="card-body text-success">
                         <h1 className="text-center">123</h1>
                         <progress id="file" value="32" max="100"> 32% </progress>
                     </div>
-                    <div class="card-footer bg-transparent">12gb out of 15gb is used</div>
+                    <div className="card-footer bg-transparent">12gb out of 15gb is used</div>
                 </div>
             </div>
             
