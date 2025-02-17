7b9d42936ccf79f89757acc5664a60745891a442
Yuyi-Hao <roshanisingh2354@gmail.com>
2025-02-17 23:07:11 +0530

    Login and signup form

--- src/components/user_auth.jsx
+++ src/components/user_auth.jsx
@@ -39,27 +39,44 @@ function UserAuthForm({ route, method }) {
     }
     const labelClasses = "block";
     return (
-        <form onSubmit={handleFormSubmission} className="flex flex-col gap-y5" method="POST">
-            <h1>{formName}</h1>
-            <div className="flex flex-col gap-y-5">
-                <label className={labelClasses} htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="user@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-                {(method === "register") ?
-                    <>
-                        <label className={labelClasses} htmlFor="username">Username</label>
-                        <input type="text" name="username" id="username" placeholder="Your Name" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                        <label className={labelClasses} htmlFor="date-of-birth">Date of Birth</label>
-                        <input type="date" name="date-of-birth" id="date-of-birth" placeholder="YYYY-MM-DD" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)}/>
-                    </> :
-                    <></>}
-                <label className={labelClasses} htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                {(method === "register") ?
-                    <>
-                        <label className={labelClasses} htmlFor="confirm_password">Confirm Password</label>
-                        <input type="password" name="confirm_password" id="confirm_password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/></> : <></>}
+        <form onSubmit={handleFormSubmission} className="container py-5 h-100" method="POST">
+            <h1 className="text-center">{formName}</h1>
+            <div className="form-outline mb-4">
+                <label htmlFor="email" className="form-label">Email</label>
+                <input type="email" name="email" id="email" className="form-control" placeholder="user@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
-            <button type="submit" className="btn btn-primary">{formName}</button>
+            {(method === "register") ?
+                <>
+                    <div className="form-outline mb-4">
+                        <label className="form-label" htmlFor="username">Username</label>
+                        <input className="form-control" type="text" name="username" id="username" placeholder="Your Name" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+                    <div className="form-outline mb-4">
+                        <label className="form-label" htmlFor="date-of-birth">Date of Birth</label>
+                        <input className="form-control" type="date" name="date-of-birth" id="date-of-birth" placeholder="YYYY-MM-DD" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)}/>
+                    </div>
+                </> :
+                <></>} 
+            <div className="form-outline mb-4">
+                <label className="form-label" htmlFor="password">Password</label>
+                <input className="form-control" type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            {(method === "register") ?
+                <>
+                    <div className="form-outline mb-4">
+                        <label className="form-label" htmlFor="confirm_password">Confirm Password</label>
+                        <input className="form-control" type="password" name="confirm_password" id="confirm_password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                    </div>
+                </> : <></>}
+            <button type="submit" className="btn btn-primary btn-block mb-4">{formName}</button>
+            {(method === "register") ?
+                <>
+                    <p className="text-center">Already have an account? <a href="/accounts/login">Sign In</a></p>
+                </> : 
+                <>
+                    <p className="text-center">Not a member? <a href="/accounts/signup">Sign Up</a></p>
+                </>
+            }
         </form>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,9 @@ function Home(){
     <div>
         {/* Navbar */}
         <NavBar/>
-        <h1>Home</h1>
-        
-        <a href="/accounts/login">Login</a>
-        <a href="/accounts/signup">Signup</a>
-        <a href="/accounts/logout">Logout</a>
-        <a href="/akdkahdhskj">404</a>
+        <div className="container">
+            <h1 className="text-center">COL DOC</h1>
+        </div>
     </div>
     )
 }
